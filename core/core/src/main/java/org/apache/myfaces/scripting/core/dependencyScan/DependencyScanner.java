e17410fbdb16b043f2a8fa4c3e4ee087dcd7962e
Werner Punz <werpu@apache.org>
2010-03-02 21:35:27 +0000

    https://issues.apache.org/jira/browse/EXTSCRIPT-81
    adding comments to our main interface
    
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/scripting/trunk@918199 13f79535-47bb-0310-9956-ffa450edef68

--- core/core/src/main/java/org/apache/myfaces/scripting/core/dependencyScan/DependencyScanner.java
+++ core/core/src/main/java/org/apache/myfaces/scripting/core/dependencyScan/DependencyScanner.java
@@ -1,15 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.myfaces.scripting.core.dependencyScan;
 
 import org.apache.myfaces.scripting.core.dependencyScan.api.DependencyRegistry;
 import org.apache.myfaces.scripting.core.dependencyScan.registry.ExternalFilterDependencyRegistry;
 
 /**
- * Created by IntelliJ IDEA.
- * User: werpu2
- * Date: 01.03.2010
- * Time: 12:02:20
- * To change this template use File | Settings | File Templates.
+ * Standard dependency scanner interface
+ *
+ * @author Werner Punz (latest modification by $Author$)
+ * @version $Revision$ $Date$
  */
 public interface DependencyScanner {
+    /**
+     * main method every dependency scanner has to implement
+     *
+     * @param loader     the classloader which is able to serve the requested class resources
+     * @param engineType integer value of the scanning triggering engine type
+     * @param className  of the class to be scanned
+     * @param registry   the registry which should receive the results of the scan
+     */
     void fetchDependencies(ClassLoader loader, Integer engineType, String className, DependencyRegistry registry);
 }
