eaf13f3c3347173a4f8794a77e4b65bb7b4d4a1e
Werner Punz <werpu@apache.org>
2010-03-09 14:47:10 +0000

    https://issues.apache.org/jira/browse/EXTSCRIPT-90
    introducing meta bundles to reduce setup complexity for the end user!
    
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/scripting/trunk@920900 13f79535-47bb-0310-9956-ffa450edef68

--- examples/myfaces20-example/src/main/java/org/apache/myfaces/otherEngines/TestBeanImpl.java
+++ examples/myfaces20-example/src/main/java/org/apache/myfaces/otherEngines/TestBeanImpl.java
@@ -1,7 +1,5 @@
 package org.apache.myfaces.otherEngines;
 
-import org.apache.myfaces.scripting.core.util.FileUtils;
-
 import javax.faces.FacesException;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -19,7 +17,11 @@ public class TestBeanImpl implements TestBean {
         try {
             String resourceRoot = this.getClass().getClassLoader().getResource("./").getFile();
             //dirty and only works if you run it in maven jetty:run but for the demo this is ok
-            String resource = resourceRoot+"../../../../src/main/java/"+"org.apache.myfaces.otherEngines".replaceAll("\\.", FileUtils.getFileSeparatorForRegex())+File.separator+"TestBean.js";
+            String separator = File.separator;
+            if(separator.equals("\\")) {
+                separator = separator+separator;
+            }
+            String resource = resourceRoot+"../../../../src/main/java/"+"org.apache.myfaces.otherEngines".replaceAll("\\.", separator)+"TestBean.js";
             _delegate = (TestBean) JavascriptProxyFactory.newInstance(TestBean.class, "TestBean", new File(resource));
         } catch (ScriptException e) {
             throw new FacesException(e);
