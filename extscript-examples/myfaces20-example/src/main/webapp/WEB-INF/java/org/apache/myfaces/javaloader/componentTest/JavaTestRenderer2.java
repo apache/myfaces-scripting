b11bf204f4673b63175f955bf07ee28bb5b518de
Werner Punz <werpu@apache.org>
2010-05-05 15:26:01 +0000

    https://issues.apache.org/jira/browse/EXTSCRIPT-130
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/scripting/trunk@941343 13f79535-47bb-0310-9956-ffa450edef68

--- extscript-examples/myfaces20-example/src/main/webapp/WEB-INF/java/org/apache/myfaces/javaloader/componentTest/JavaTestRenderer2.java
+++ extscript-examples/myfaces20-example/src/main/webapp/WEB-INF/java/org/apache/myfaces/javaloader/componentTest/JavaTestRenderer2.java
@@ -32,11 +32,12 @@ import org.apache.myfaces.shared_impl.renderkit.html.HtmlTextareaRendererBase;
  * @author Werner Punz (latest modification by $Author$)
  * @version $Revision$ $Date$
  */
+@FacesRenderer(componentFamily = "javax.faces.Input", rendererType = "at.irian.JavaTestRenderer")
 
 public class JavaTestRenderer2 extends HtmlTextareaRendererBase {
 
-    private static final String MSG = "<h2> Hello world from Renderer 2 </h2>";
-    private static final String MSG2 = "<h3> hello world second var <h3>";
+    private static final String MSG = "<h2> Hello world ccc from Renderer 2 </h2>";
+    private static final String MSG2 = "<h3> hello world second var xxx<h3>";
 
 
     @Override
