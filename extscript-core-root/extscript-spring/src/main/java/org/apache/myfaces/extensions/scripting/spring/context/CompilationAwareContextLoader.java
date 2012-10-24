cf673b095f7fe114b2f2f8ded64060a6a4414f3a
Werner Punz <werpu@apache.org>
2012-10-24 13:48:59 +0000

    save state of affairs webapp now shot boot, and also ext-scripting
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/scripting/trunk@1401685 13f79535-47bb-0310-9956-ffa450edef68

---
+++ extscript-core-root/extscript-spring/src/main/java/org/apache/myfaces/extensions/scripting/spring/context/CompilationAwareContextLoader.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.myfaces.extensions.scripting.spring.context;
+
+import org.apache.myfaces.extensions.scripting.core.api.WeavingContext;
+import org.apache.myfaces.extensions.scripting.jsf.startup.StartupServletContextPluginChainLoader;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.ServletContext;
+import java.io.IOException;
+
+/**
+ * @author Werner Punz (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+
+public class CompilationAwareContextLoader extends ContextLoader
+{
+    private static final String RELOADING_LISTENER = "ReloadingListener";
+
+    @Override
+    protected WebApplicationContext createWebApplicationContext(
+            ServletContext servletContext, ApplicationContext parent) throws BeansException
+    {
+        WebApplicationContext retVal = super.createWebApplicationContext(servletContext, parent);
+        //we now init the scripting system
+        try
+        {
+            //the reloading listener also is the marker to avoid double initialisation
+            //after the container is kickstarted
+            if (servletContext.getAttribute(RELOADING_LISTENER) == null)
+            {
+                StartupServletContextPluginChainLoader.startup(servletContext);
+                servletContext.setAttribute(RELOADING_LISTENER, new ReloadingListener());
+                WeavingContext.getInstance().addListener((ReloadingListener) servletContext.getAttribute(RELOADING_LISTENER));
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return retVal;
+    }
+}
---
+++ extscript-core-root/extscript-spring/src/main/java/org/apache/myfaces/extensions/scripting/spring/context/CompilationAwareContextLoaderListener.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.myfaces.extensions.scripting.spring.context;
+
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.ContextLoaderListener;
+
+/**
+ * @author Werner Punz (latest modification by $Author$)
+ * @version $Revision$ $Date$
+ */
+
+public class CompilationAwareContextLoaderListener extends ContextLoaderListener
+{
+    /**
+        * <p>Creates the context loader to use. Note that actually the context loader
+        * starts up the web application context, this listener just delgates
+        * to it.</p>
+        *
+        * @return the context loader to use to start up the web appplication context
+        */
+       @Override
+       protected ContextLoader createContextLoader() {
+           return new CompilationAwareContextLoader();
+       }
+}
