454337c6095128b35a70ca375be33b9bdc5d7589
Werner Punz <werpu@apache.org>
2012-10-25 07:52:16 +0000

    EXTSCRIPT-44: testprobe1 accidentally overwritten
    git-svn-id: https://svn.apache.org/repos/asf/myfaces/extensions/scripting/trunk@1402003 13f79535-47bb-0310-9956-ffa450edef68

--- extscript-core-root/extscript-core/src/test/resources/compiler/TestProbe1.java
+++ extscript-core-root/extscript-core/src/test/resources/compiler/TestProbe1.java
@@ -1,84 +1,36 @@
-import org.jruby.Ruby;
-import org.jruby.RubyClass;
-import org.jruby.RubyObject;
-import org.jruby.javasupport.util.RuntimeHelpers;
-import org.jruby.runtime.builtin.IRubyObject;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package compiler;
 
+/**
+ * a simple testprobe to check if the compiler api works
+ */
 
-public class TestProbe1 extends RubyObject  {
-    private static final Ruby __ruby__ = Ruby.getGlobalRuntime();
-    private static final RubyClass __metaclass__;
+public class TestProbe1 {
 
-    static {
-        String source = new StringBuilder("# a small ruby testing class to test for a valid compilation\n" +
-            "# from the compiler\n" +
-            "# @author Werner Punz\n" +
-            "class TestProbe1\n" +
-            "  java_signature 'void helloWorld()'\n" +
-            "  def helloWorld\n" +
-            "      puts \"Hello from Ruby\"\n" +
-            "  end\n" +
-            "\n" +
-            "  java_signature 'public String stringReturn()'\n" +
-            "  def stringReturn\n" +
-            "    'hello world'\n" +
-            "  end\n" +
-            "\n" +
-            "end\n" +
-            "").toString();
-        __ruby__.executeScript(source, "TestProbe1JRuby.rb");
-        RubyClass metaclass = __ruby__.getClass("TestProbe1");
-        metaclass.setRubyStaticAllocator(TestProbe1.class);
-        if (metaclass == null) throw new NoClassDefFoundError("Could not load Ruby class: TestProbe1");
-        __metaclass__ = metaclass;
+    private String testAttr;
+
+    public String getTestAttr() {
+        return testAttr;
     }
 
-    /**
-     * Standard Ruby object constructor, for construction-from-Ruby purposes.
-     * Generally not for user consumption.
-     *
-     * @param ruby The JRuby instance this object will belong to
-     * @param metaclass The RubyClass representing the Ruby class of this object
-     */
-    private TestProbe1(Ruby ruby, RubyClass metaclass) {
-        super(ruby, metaclass);
+    public void setTestAttr(String testAttr) {
+        this.testAttr = testAttr;
     }
-
-    /**
-     * A static method used by JRuby for allocating instances of this object
-     * from Ruby. Generally not for user comsumption.
-     *
-     * @param ruby The JRuby instance this object will belong to
-     * @param metaclass The RubyClass representing the Ruby class of this object
-     */
-    public static IRubyObject __allocate__(Ruby ruby, RubyClass metaClass) {
-        return new TestProbe1(ruby, metaClass);
-    }
-        
-    /**
-     * Default constructor. Invokes this(Ruby, RubyClass) with the classloader-static
-     * Ruby and RubyClass instances assocated with this class, and then invokes the
-     * no-argument 'initialize' method in Ruby.
-     */
-    public TestProbe1() {
-        this(__ruby__, __metaclass__);
-        RuntimeHelpers.invoke(__ruby__.getCurrentContext(), this, "initialize");
-    }
-
-    
-    public void helloWorld() {
-
-        IRubyObject ruby_result = RuntimeHelpers.invoke(__ruby__.getCurrentContext(), this, "helloWorld");
-        return;
-
-    }
-
-    
-    public String stringReturn() {
-
-        IRubyObject ruby_result = RuntimeHelpers.invoke(__ruby__.getCurrentContext(), this, "stringReturn");
-        return (String)ruby_result.toJava(String.class);
-
-    }
-
 }
